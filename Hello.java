4c76582d376b88a14571047e0f9dc30a35514fa3
Aizat72 <aizat2006.g@gmail.com>
2026-01-05 01:08:21 +0300

    Add second print line

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Git!");
         System.out.println("Hello, Git and GitHub!");
+        System.out.println("End of demo.");
     }
 }
